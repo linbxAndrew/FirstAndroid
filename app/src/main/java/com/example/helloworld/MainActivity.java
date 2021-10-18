ab1a2f21e8f31399c9fc2d8a1990cb3acfa37b7e
linbxAndrew <3133604904@qq.com>
2021-10-18 16:13:24 +0800

    three condition

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // activity_main linear_layout_9_18 constraint_layout_9_18 table_layout_9_18
+    }
+
+    public void clickHandler(View source) {
+        // Get the text box with ID R.layout.show in UI interface
+        TextView tv = findViewById(R.id.show);
+        //change the text content of the text box
+        tv.setText(R.string.app_and + "-" + new java.util.Date());
+
     }
 }
